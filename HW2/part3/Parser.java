0ef5dd32b7102e025a6c12778a6bdf486d71cb5f
Hanh Kieu <hanh@ennetix.com>
2016-01-26 02:21:40 -0800

    cleaned up code

--- HW2/part3/Parser.java
+++ HW2/part3/Parser.java
@@ -4,16 +4,12 @@ import java.util.ArrayList;
 
 public class Parser {
 
-
     // tok is global to all these parsing methods;
     // scan just calls the scanner's scan method and saves the result in tok.
     private Token tok; // the current token
     private SymbolTable myTable = new SymbolTable();
     List<String> scope = new ArrayList<String>();
     int counter = 0;
-
-
-    //myTable.myStack.push();
     
     private void scan() {
 	   tok = scanner.scan();
@@ -31,11 +27,10 @@ public class Parser {
     }
 
     private void program() {
-	block();
+	   block();
     }
 
     private void block(){
-        //System.out.println("(block) 	 " + tok.string );
         if(counter != 0 && (!scope.isEmpty()))
         {
             myTable.myStack.push(scope);
@@ -45,30 +40,25 @@ public class Parser {
 
     	declaration_list();
     	statement_list();
+
         if(!myTable.myStack.empty()){
             System.out.println(myTable.myStack.pop() + "boobs");
         }
-    }//COMPLETED
+    }
 
-    private void declaration_list() {
-        //System.out.println("(declaration_list) 	 " + tok.string );
-	// below checks whether tok is in first set of declaration.
-	// here, that's easy since there's only one token kind in the set.
-	// in other places, though, there might be more.
-	// so, you might want to write a general function to handle that.
+    private void declaration_list() {  
 		while( is(TK.DECLARE) ) {
 		    declaration();
 		}
-    }//completed
+    }
 
     private void declaration() {
-        //System.out.println("(declaration) 	 " + tok.string );
 		mustbe(TK.DECLARE);
         if(is(TK.ID)){
             if(notDeclared(tok.string))
                 scope.add(tok.string);
             else{
-                redeclaration_error(tok.string,tok.lineNumber);
+                redeclaration_error(tok.string);
             }
         }
 		mustbe(TK.ID);
@@ -79,15 +69,14 @@ public class Parser {
                 if(notDeclared(tok.string))
                     scope.add(tok.string);
                 else{
-                    redeclaration_error(tok.string,tok.lineNumber);
+                    redeclaration_error(tok.string);
                 }
             }
 		    mustbe(TK.ID);
 		}
-    }//COMPLETED
+    }
 
     private void statement(){
-        //System.out.println("(statement) 	 " + tok.string );
     	if(is(TK.TILDE) || is(TK.ID))
     		assignment();
     	else if(is(TK.PRINT))
@@ -97,50 +86,44 @@ public class Parser {
     	}
     	else if(is(TK.IF))
     		iff();
-    }//COMPLETED
+    }
 
     private void statement_list() {
-        //System.out.println("(statement_list) 	 " + tok.string );
     	while( is(TK.TILDE) || is(TK.ID) || is(TK.PRINT) || is(TK.DO) || is(TK.IF)){
     		statement();
     	}
-    }//COMPLETED
+    }
 
     private void print(){
-        //System.out.println("(print) 	 " + tok.string );
     	mustbe(TK.PRINT);
     	expr();
-    }//COMPLETED
+    }
 
     private void assignment(){
-        //System.out.println("(assignment) 	 " + tok.string );
     	ref_id();
     	mustbe(TK.ASSIGN);
     	expr();
-    }//COMPLETED
+    }
 
-    private void ref_id(){
-        //System.out.println("(ref_id) 	 " + tok.string );
+    private void ref_id(){ 
     	if(is(TK.TILDE)){
     		mustbe(TK.TILDE);
     		if(is(TK.NUM))
     			mustbe(TK.NUM);
     	}
 
-        if(notDeclared(tok.string))
-            declaration_error(tok.string,tok.lineNumber);
+        // if(notDeclared(tok.string))
+        //     declaration_error(tok.string,tok.lineNumber);
     	mustbe(TK.ID);
-    }//COMPLETED
+    }
 
     private void doo(){
-        //System.out.println("(doo) 	 " + tok.string );
     	mustbe(TK.DO);
     	guarded_command();
     	mustbe(TK.ENDDO);
-    }//COMPLETED
+    }
 
     private void iff(){
-        //System.out.println("(iff) 	 " + tok.string );
     	mustbe(TK.IF);
     	guarded_command();
     	while(is(TK.ELSEIF)) {
@@ -152,35 +135,31 @@ public class Parser {
     		block();
     	}
     	mustbe(TK.ENDIF);
-    }//COMPLETED
+    }
 
-    private void guarded_command(){
-        //System.out.println("(guarded_command) 	 " + tok.string );
+    private void guarded_command(){   
     	expr();
     	mustbe(TK.THEN);
     	block();
-    }//COMPLETED
+    }
 
-    private void expr(){
-        //System.out.println("(expr) 	 " + tok.string );
+    private void expr(){ 
     	term();
     	while( is(TK.PLUS) || is(TK.MINUS) ) {
 	    	scan();
 	    	term();
 		}
-    }//COMPLETED
+    }
 
-    private void term(){
-        //System.out.println("(term) 	 " + tok.string );
+    private void term(){  
     	factor();
     	while(is(TK.TIMES) || is(TK.DIVIDE)){
     		scan();
     		factor();
     	}
-    }//COMPLETED
+    }
 
-    private void factor(){
-        //System.out.println("(factor) 	 " + tok.string );
+    private void factor(){    
     	if(is(TK.LPAREN)){
     		mustbe(TK.LPAREN);
     		expr();
@@ -193,8 +172,7 @@ public class Parser {
     	{
     		mustbe(TK.NUM);
     	}
-
-    }//COMPLETED
+    }
 
     // is current token what we want?
     private boolean is(TK tk) {
@@ -210,6 +188,7 @@ public class Parser {
 	}
 	scan();
     }
+
     /*
     //If the id has not been declared yet return true
     //else return false
@@ -237,9 +216,8 @@ public class Parser {
     //string is the id that is being redeclared
     //linenumber is the line in which it occured
     */
-    private void redeclaration_error(String string, int lineNumber){
-        System.err.println(string + " is a redeclared variable on line " + lineNumber);
-        System.exit(1);
+    private void redeclaration_error(String string){
+        System.err.println("redeclaration of variable " + string);
     }
 
     private void parse_error(String msg) {
